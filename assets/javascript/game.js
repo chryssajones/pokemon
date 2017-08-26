55bda1c3ee3b8476f62960921b829fbb246a558e
Chryssa <chryssajones@gmail.com>
2017-08-25 23:38:00 -0700

    in progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+console.log("JavaScript is running");
+
+var pokemon = {
+	pikachu: ["Pikachu", "Thunderbolt", "Quick Attack", 55, 20], 
+	squirtle: ["Squirtle", src="../images/squirtle.png", "Bubble", "Water Gun", 40, 60],
+	bulbasaur: ["Bulbasaur", src="../images/bulbasaur.png", "Razor Leaf", "Seed Bomb", 55, 80],
+	charmander: ["Charmander", src="../images/charmander.png", "Ember", "Flamethrower", 30, 75],
+	jigglypuff: ["Jigglypuff", src="../images/jigglypuff.png", "Double Slap", "Body Slam", 15, 65],
+}
+
+
